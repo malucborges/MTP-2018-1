bbb5adac4efed62eedd8074d5d4010396de23015
malucborges <38254808+malucborges@users.noreply.github.com>
2018-04-16 19:46:49 -0300

    Add files via upload

---
+++ P1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+	char bits[256];
+	int estado, aut, i;
+	printf("Insira um numero binario: ");
+	scanf("%s", &bits);
+	for(i = 0; bits [i] != '\0'; i++)
+	{
+		if (estado == 0 && bits [i] == '1')
+			aut = 1;
+		if (estado == 0 && bits [i] == '0')
+			aut = 0;
+		if (estado == 1 && bits [i] == '1')
+			aut = 0;
+		if (estado == 1 && bits [i] == '0')
+			aut = 2;
+		if (estado == 2 && bits [i] == '1')
+			aut = 2;
+		if (estado == 2 && bits [i] == '0')
+			aut = 1;
+	estado = aut;
+	aut = 0;
+	}
+	if (estado == 0)
+		printf("\nO numero binario %s e multiplo de 3.", bits);
+	else 
+		printf("\nO numero binario %s nao e multiplo de 3.", bits);
+	return 0;
+}
