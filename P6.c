071b6d9fdc0636c107bec3da38bc0966683d2e16
malucborges <38254808+malucborges@users.noreply.github.com>
2018-05-25 18:43:37 -0300

    Create P6.c

---
+++ P6.c
@@ -0,0 +1,52 @@
+//Maria Luiza Camargos Borges Carneiro
+//11721EBI002
+
+#include <stdio.h>
+
+int soma(int vet[20], int n)
+{
+	if(n == 0)
+		return 0;
+	else 
+	{
+		int s;
+		s = soma (vet, n-1);
+		if(vet[n-1] > 0)
+			s = s + vet[n-1];
+		return (s);
+	}
+}
+
+float media(int soma, float p)
+{
+	float res;
+	res = soma/p;
+	return res;
+}
+
+void preenche(int vet[20])
+{
+	int i;
+	for(i = 0; i < 20; i++)
+		vet[i] = 0;
+}
+
+int main ()
+{
+	int vet[20], i, sominha, num;
+	float p;
+	printf("Insira a quantidade de numeros inteiros: ");
+	scanf("%d", &num);
+	preenche(vet);
+	for(i = 0; i < num; i++)
+	{
+		printf("\nDigite o numero %d: ", i+1);
+		scanf("%d", &vet[i]);
+		getchar();
+		if(vet[i] != 0)
+			p++;
+	}
+	sominha = (soma(vet, i));
+	printf("\nA media dos numeros e: %.1f", media(sominha, p));
+	return 0;
+}
